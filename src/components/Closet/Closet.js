9165e319b6c07f432516dd1f89a6a65006d9a62f
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-16 18:56:13 -0400

    Switch category component to links

--- src/components/Closet/Closet.js
+++ src/components/Closet/Closet.js
@@ -1,16 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
-import Category from '../Category/Category';
+import { Link } from 'react-router-dom';
 import './Closet.css'
 
 const Closet = () => {
   const categories = ['Pants', 'Skirts', 'Shirts', 'One Pieces', 'Outer Wear', 'Bags', 'Shoes', 'Accessories', 'Other']
   const categoryEls = categories.map(category => {
     const id = uuidv4();
-   return <Category key={id} id={id} name={category}/>
+    const linkLocation = category.split(' ').join('').toLowerCase()
+    return <Link to={`/closet/${linkLocation}`} className='category' key={id} id={id} >{category}</Link>
   })
 
   return (
     <section className='closet-page'>
+      <h1 className='page-title' >My Closet</h1>
       <div className='category-container'>
         {categoryEls}
       </div>
