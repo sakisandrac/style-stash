522e29d9821afc2145b0b18acbf092101af0f8b9
“Saki <sakisandra.c@gmail.com>
2023-07-16 12:18:17 -0600

    Add basic form component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from '../NavBar/NavBar'
+import ItemForm from '../ItemForm/ItemForm';
 
 function App() {
   return (
     <main>
       <NavBar /> 
+      <ItemForm />
     </main>
   );
 }
---
+++ src/components/ItemForm/ItemForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './ItemForm.css'
+
+const ItemForm = () => {
+  const [imgFile, setImgFile] = useState();
+  const [notes, setNotes] = useState();
+  const [category, setCategory] = useState();
+
+  const [newData, setNewData] = useState({
+    id: "",
+    image: "",
+    category:"",
+    notes: ""
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.files);
+    setImgFile(URL.createObjectURL(e.target.files[0]));
+    console.log('file state', URL.createObjectURL(e.target.files[0]))
+}
+
+  const handleSelect = (e) => {
+    setCategory(e.target.value)
+  }
+
+  return ( 
+    <div className='item-form'>
+      <h2>Add Item:</h2>
+      <form>
+      <input type="file" onChange={handleChange} />
+      <img className='file-image' src={imgFile} />
+      <textarea placeholder='notes'></textarea>
+      <select
+        value={category}
+        onChange={(e) => {handleSelect(e)}}
+        name="category">
+        <option>Please Select one:</option>
+        <option value='pants'>Pants</option>
+        <option value='skirts'>Skirts</option>
+        <option value='onePieces'>One-Pieces</option>
+        <option value='tops'>Tops</option>
+        <option value='outerwear'>Outerwear</option>
+        <option value='shoes'>Shoes</option>
+        <option value='bags'>Bags</option>
+        <option value='accessories'>Accessories</option>
+        <option value='misc'>Miscellaneous</option>
+      </select>
+      <button className='add-btn'>Add Item</button>
+      </form>
+
+      
+    </div>
+  )
+}
+
+export default ItemForm
