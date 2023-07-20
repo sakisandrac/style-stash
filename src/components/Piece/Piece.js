2e8fa132f815ef3df690254de2ac15fb57a8f97b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-19 23:48:10 -0400

    Add text area for edit peice

--- src/components/Piece/Piece.js
+++ src/components/Piece/Piece.js
@@ -6,21 +6,32 @@ import './Piece.css'
 
 const Piece = ({user}) => {
   const [piece, setPiece] = useState('')
+  const [editing, setEditing] = useState(false)
+  const [pieceNotes, setPieceNotes] = useState('')
   const {pieceID, category} = useParams()
 
   useEffect(() => {
+    console.log('mounting...')
     const apiCall = async () => {
       try {
-        console.log(user)
         const fetchedCategory = await getClosetData(category, user.userID)
         const fetchedPiece = fetchedCategory.filteredPieces.find(item => item.id === pieceID)
         setPiece(fetchedPiece)
+        console.log(fetchedPiece)
       } catch(error) {
         console.log(error)
       }
     }
     apiCall()
   }, [])
+
+  useEffect(() => {
+    setPieceNotes(piece.notes)
+  }, [piece])
+
+  useEffect(() => {
+    console.log('editing', editing)
+  }, [editing])
 
 
   return (
@@ -29,10 +40,11 @@ const Piece = ({user}) => {
       <section className="cart-pieces clothing-container">
         <img src={piece.image} alt={`clothing item from ${category} category`}/> 
       </section>
-      <article className={piece.notes ? "outfit-notes piece-notes" : "outfit-notes piece-notes no-note"}>
+      {editing ? <input className='outfit-notes' type='textarea' placeholder="Add notes for this outfit..." value={pieceNotes} onChange={(e) => setPieceNotes(e.target.value)}/>
+      : <article className={piece.notes ? "outfit-notes piece-notes" : "outfit-notes piece-notes no-note"}>
         {piece.notes ? piece.notes : 'Edit item to add notes'}
-      </article>
-      <button className="cart-button" id="editBtn">EDIT ITEM</button>
+      </article>}
+      <button className="cart-button" id="editBtn" onClick={() => setEditing(prev => !prev)}>{editing ? 'SAVE' : 'EDIT'} ITEM</button>
     </section>
   )
 }
