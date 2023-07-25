f07734fb083fc411d9565be0c9e563194bb66e0a
“Saki <sakisandra.c@gmail.com>
2023-07-24 22:39:09 -0600

    Add button to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,8 +36,8 @@ const Home = ({user, setAppError}) => {
           {/* <div className="overlay1"></div> */}
           <div className='view-outfit-btn'>View my outfit</div>
           {/* this doesn't link anywhere yet- will have to come back and add it! */}
-          <p className='featured-img-text'>Your Featured Outfit!</p>
-        </div>
+          <p className='featured-img-text'>♡ Today's Featured Outfit ♡</p>
+        </div> 
         : <p>Please Login to Style Stash!</p>}
       </main>
     </div>
